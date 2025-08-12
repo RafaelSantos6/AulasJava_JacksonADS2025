6a6d927a3eb84f06989774bbd33b02fde3235923
RAFAEL CRISTIANO RICETTI DOS SANTOS <rafael.santos11@alunos.sc.senac.br>
2025-08-11 22:09:15 -0300

    J

---
+++ ArraySimples.java
@@ -0,0 +1,5 @@
+public class ArraySimples {
+
+    public static void main(String[] args) {
+    }
+}
--- jogoDaVelha.java
+++ jogoDaVelha.java
@@ -1,7 +1,91 @@
+import java.util.Scanner;
 
 public class jogoDaVelha {
+    static char[][] tabuleiro = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    };
+
+    static char jogadorAtual = 'X';
 
     public static void main(String[] args) {
-      
+        Scanner scanner = new Scanner(System.in);
+        boolean jogoAtivo = true;
+
+        while (jogoAtivo) {
+            mostrarTabuleiro();
+            System.out.println("Jogador " + jogadorAtual + ", informe linha e coluna (0, 1 ou 2): ");
+            int linha = scanner.nextInt();
+            int coluna = scanner.nextInt();
+
+            if (linha < 0 || linha > 2 || coluna < 0 || coluna > 2) {
+                System.out.println("Coordenadas inválidas. Tente novamente.");
+                continue;
+            }
+
+            if (tabuleiro[linha][coluna] != ' ') {
+                System.out.println("Posição já ocupada. Tente novamente.");
+                continue;
+            }
+
+            tabuleiro[linha][coluna] = jogadorAtual;
+
+            if (verificarVencedor()) {
+                mostrarTabuleiro();
+                System.out.println("Jogador " + jogadorAtual + " venceu!");
+                jogoAtivo = false;
+            } else if (tabuleiroCheio()) {
+                mostrarTabuleiro();
+                System.out.println("Empate!");
+                jogoAtivo = false;
+            } else {
+                trocarJogador();
+            }
+        }
+
+        scanner.close();
+    }
+
+    static void mostrarTabuleiro() {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(tabuleiro[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    static void trocarJogador() {
+        jogadorAtual = (jogadorAtual == 'X') ? 'O' : 'X';
+    }
+
+    static boolean tabuleiroCheio() {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (tabuleiro[i][j] == ' ')
+                    return false;
+        return true;
+    }
+
+    static boolean verificarVencedor() {
+        // Linhas e colunas
+        for (int i = 0; i < 3; i++) {
+            if ((tabuleiro[i][0] == jogadorAtual && tabuleiro[i][1] == jogadorAtual && tabuleiro[i][2] == jogadorAtual) ||
+                (tabuleiro[0][i] == jogadorAtual && tabuleiro[1][i] == jogadorAtual && tabuleiro[2][i] == jogadorAtual)) {
+                return true;
+            }
+        }
+
+        // Diagonais
+        if ((tabuleiro[0][0] == jogadorAtual && tabuleiro[1][1] == jogadorAtual && tabuleiro[2][2] == jogadorAtual) ||
+            (tabuleiro[0][2] == jogadorAtual && tabuleiro[1][1] == jogadorAtual && tabuleiro[2][0] == jogadorAtual)) {
+            return true;
+        }
+
+        return false;
     }
 }
